b1091eb1fe4e80bbeeab6829e71c323947e11132
kanikakg <kanikakg100@gmail.com>
2018-03-07 03:07:07 +0530

    Added avatar and bio to profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   has_many :pins 
 
   validates_uniqueness_of :username
+
+  has_attached_file :avatar, styles:  { medium: "100x100>", thumb: "100x100"}, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
